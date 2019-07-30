d54cdf128428827c26bff7e5e3fd152755bacf13
Muhammad Azhdari <mohammad.azhdari.22@gmail.com>
2019-07-30 19:13:11 +0430

    remove test token

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram.ext import Updater
 from bot.test_controller import TestController
 
 if __name__ == '__main__':
-    updater = Updater(token="491561648:d6506e5fcfa7a3804bba78d528445392965f2de5")
+    updater = Updater(token="YOUR_BOT_TOKEN")
     dispatcher = updater.dispatcher
     test_controller = TestController(dispatcher=dispatcher)
     updater.start_polling(poll_interval=1)
